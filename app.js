3b521ed2f2070e6211a2d0703513c9e9509fb814
nonin22222 <nonin31313@gmail.com>
2023-11-15 14:42:02 +0700

    15/11/23

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cor = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 ////
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cor())
 //router
 const prefix = '/v2/nba-hotel'
 app.use(prefix+'/', indexRouter);
@@ -38,23 +40,10 @@ app.use(prefix+'/room',require('./routes/room'))
 app.use(prefix+'/booking',require('./routes/booking'))
 app.use(prefix+'/checkin',require('./routes/checkin'))
 app.use(prefix+'/payment',require('./routes/payment'))
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // หรือกำหนด origin ที่เฉพาะเจาะจง
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 const port = process.env.PORT || 4444;
 app.listen(port, console.log(`Listening on port ${port}`));
